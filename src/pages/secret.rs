1b807d007f2cec822e2ba2b3a06b587e546232c6
rho2-pdx <rho2@pdx.edu>
2026-02-02 22:18:21 -0800

    added buttons

--- src/pages/secret.rs
+++ src/pages/secret.rs
@@ -4,13 +4,38 @@ use leptos::prelude::*;
 #[component]
 pub fn Secret() -> impl IntoView {
     let values = vec![0, 1, 2];
+    // create a list of 5 signals
+    let length = 5;
+    let counters = (1..=length).map(|idx| RwSignal::new(idx));
+    let counter_buttons = counters
+        .map(|count| {
+            view! {
+                <li>
+                    <button
+                        on:click=move |_| *count.write() += 1
+                    >
+                        {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
     view! {
+        <h1>"you found the secret page!"</h1>
         <p>{values.clone()}</p>
+        /*
         <ul>
             {values.into_iter()
                 .map(|n| view! { <li>{n}</li>})
                 .collect::<Vec<_>>()}
         </ul>
-        <h1>"you found the secret page!"</h1>
+        */
+        <ul>
+            {values.into_iter()
+                .map(|n| view! { <li>{n}</li>})
+                .collect_view()}
+        </ul>
+        <ul> {counter_buttons}</ul>
+
     }
 }
