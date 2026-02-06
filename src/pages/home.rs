32b44b46ed914a1c54f4b8842a14a792d0094f89
rho2-pdx <rho2@pdx.edu>
2026-02-05 23:41:37 -0800

    work in progress

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -26,6 +26,24 @@ pub fn Home() -> impl IntoView {
             }
         }>
 
+        let (name, set_name) = signal("Controlled".to_string());
+
+        view! {
+            <input type="text"
+                // adding :target gives us typed access to the element
+                // that is the target of the event that fires
+                on:input:target=move |ev| {
+                    // .value() returns the current value of an HTML input element
+                    set_name.set(ev.target().value());
+                }
+
+                // the `prop:` syntax lets you update a DOM property,
+                // rather than an attribute.
+                prop:value=name
+            />
+            <p>"Name is: " {name}</p>
+        }
+
             <div class="container">
 
                 <picture>
